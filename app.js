7bad24b8fbc8702bafcd3fc208e525bc9e7913c7
Kainon Dunn <ksdunnksd@gmail.com>
2024-05-21 13:18:51 -0400

    signing test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('A necessary means for a, a necessary means for a higher education'));
+app.get('/', (req, res) => res.send('Two minutes, Turkish.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
